5bf7dcc5a73471afba48e9a42f4e2ec11e1900f7
ridoq <ridoq444@gmail.com>
2024-09-09 10:09:28 +0700

    image nullable

--- database/migrations/2024_09_05_145208_create_projects_table.php
+++ database/migrations/2024_09_05_145208_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->string('name')->unique();
             $table->date('start_date');
             $table->date('end_date');
