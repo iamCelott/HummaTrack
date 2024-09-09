c54c5aeffc6b46a8e1f1a0fc70252099d79466cc
mkesyasakha <137471717+mkesyasakha@users.noreply.github.com>
2024-09-09 11:23:54 +0700

    update route departement and task

--- routes/sano.php
+++ routes/sano.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\KanbanController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::resource('kanbans', KanbanController::class);
+    Route::resource('departements', DepartmentController::class);
+    Route::resource('tasks', TaskController::class);
 });
