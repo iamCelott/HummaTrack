bcaf6aa2434d963106cd99bf970fa5fc0be25193
iamCelott <Dcviriya313@gmail.com>
2024-09-12 12:33:23 +0800

    team member ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,6 +86,11 @@
     {{-- CK Editor 5 --}}
     <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/43.1.0/ckeditor5.css" />
 
+    {{-- Jquery --}}
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body>
