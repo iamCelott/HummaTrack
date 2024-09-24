58d826316e71b5912ddf710d2eb85167c3cde97f
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-09-24 14:22:15 +0700

    calendar

--- routes/mugni.php
+++ routes/mugni.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Http\Controllers\TimeLineController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth')->group(function () {
+    Route::resource('calendar', TimeLineController::class);
+});
