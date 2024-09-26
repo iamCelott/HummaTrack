2bae620013561672a87cba8d835d8cfa3378d095
Andhika Mugni <165865178+Akusehatsekaliya@users.noreply.github.com>
2024-09-26 20:00:45 +0700

    halaman profile

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
 });
 
 require __DIR__ . '/auth.php';
